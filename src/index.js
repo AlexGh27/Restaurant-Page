00a3707db8ee1d05b77b14846eba2dec6e3df8e8
AlexGh27 <alexghioca27@gmail.com>
2022-10-11 21:48:01 +0300

    Building the tabs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import printMe from "./print.js";
+import './style.css';
+
+const content = document.querySelector("#content");
+
+const navbar = document.createElement("div");
+navbar.className = "navbar";
+content.appendChild(navbar);
+
+const home = document.createElement("div");
+home.className = "home";
+navbar.appendChild(home);
+
+const menu = document.createElement("div");
+menu.className = "menu";
+navbar.appendChild(menu);
+
+const contact = document.createElement("div");
+contact.className = "contact";
+navbar.appendChild(contact);
+
 
 printMe();
--- src/print.js
+++ src/print.js
@@ -1,3 +1,8 @@
 export default function printMe() {
-    console.log('I get called from print.js!');
+    
+
+
+
+
+  
   }
