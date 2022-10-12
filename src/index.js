68eabcd3652c511654fca2495e4d7cc5da442150
AlexGh27 <alexghioca27@gmail.com>
2022-10-12 09:42:33 +0300

    Structuring the home page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import printMe from "./print.js";
 import './style.css';
+import printHome from "./printHome.js";
+import printMenu from "./printMenu.js";
+import printContact from "./printContact.js";
 
 const content = document.querySelector("#content");
 
@@ -9,15 +11,38 @@ content.appendChild(navbar);
 
 const home = document.createElement("div");
 home.className = "home";
+home.innerHTML = "Home";
 navbar.appendChild(home);
+
+home.addEventListener("click", function(e) {
+    clearContent();
+    printHome();
+})
+
 
 const menu = document.createElement("div");
 menu.className = "menu";
+menu.innerHTML = "Menu";
 navbar.appendChild(menu);
+
+menu.addEventListener("click", function(e) {
+    clearContent();
+    printMenu();
+})
+
 
 const contact = document.createElement("div");
 contact.className = "contact";
+contact.innerHTML = "Contact";
 navbar.appendChild(contact);
 
+contact.addEventListener("click", function(e) {
+    clearContent();
+    printContact();
+})
 
-printMe();
+function clearContent() {
+    document.querySelector("#content").innerHTML = "";
+}
+
+printHome();
